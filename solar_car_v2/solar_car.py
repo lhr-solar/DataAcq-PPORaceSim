9145f89a1aa99026e5b116fc8aaa375bcbc04c57
Lance Ellis <scdelance@gmail.com>
2025-02-15 22:40:40 +0000

    xadsadssa Co-authored-by: Amanda Zhang <asz4817@users.noreply.github.com> Co-authored-by: Sohan Subudhi <SohanSubudhi@users.noreply.github.com> Co-authored-by: viqckhyee <viqckhyee@users.noreply.github.com>

--- solar_car_v2/solar_car.py
+++ solar_car_v2/solar_car.py
@@ -40,12 +40,12 @@ import pychrono as chrono
 from pychrono import vehicle as veh
 from pychrono import irrlicht
 
+# Standard Python imports
+import numpy as np
+
 # Gym chrono imports
 # Custom imports
 from .chrono_base import ChronoBaseEnv
-
-# Standard Python imports
-import numpy as np
 
 # Gymnasium imports
 import gymnasium as gym
@@ -139,10 +139,8 @@ class SolarCar(ChronoBaseEnv):
         self.vis = None
         self.array = None
         self.battery = None
+        self.prev_SOC = 0
         self.voltage = 0.5  # UPDATE VALUE??
-        self.power = None
-        self.current = None
-        self.energy_gen = None
 
         # ---------------------------------
         # Gym Environment variables
@@ -355,7 +353,7 @@ class SolarCar(ChronoBaseEnv):
         """Get the reward for the current step
 
         Get the reward for the current step based on the distance to the goal, and the distance the robot has traveled.
-//
+
         Returns:
             float: Reward for the current step
         """
